bfccbf134a58456cd21e154ec86588074217cdfb
noyonalways <noyonrahman2003@gmail.com>
2024-10-08 20:57:15 +0600

    update the cors

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,7 +13,20 @@ const app = express();
 // middlewares
 app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://192.168.0.116:3000",
+      "https://tech-tips-hub.vercel.app",
+      "https://tech-tips-hub.noyonrahman.xyz",
+      "https://tech-tips-hub-reset-password.vercel.app",
+      "https://tech-tips-hub-reset-password.noyonrahman.xyz",
+    ],
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
